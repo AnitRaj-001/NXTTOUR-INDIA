b825a5d3ab9a88c828433eb301ca49a2457e216d
Anit <anitraj862002@gmail.com>
2024-10-09 22:19:27 +0530

    trail commit

---
+++ lib/model/Repository.dart
@@ -0,0 +1,15 @@
+import 'package:nxtour/model/place_model.dart';
+
+import 'http_service.dart';
+
+class PlacesRepository {
+  final ApiService apiService;
+
+  PlacesRepository(this.apiService);
+
+  Future<List<Place>> getPlaces() async {
+    return await apiService.fetchPlaces();
+  }
+
+// Add methods for POST, PUT, DELETE if needed
+}
